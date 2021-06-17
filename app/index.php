c675fded3bffb781e2601503a53ebfdfd9386d41
levelsio <7150848+levelsio@users.noreply.github.com>
2021-06-17 17:25:35 +0700

    x

--- app/index.php
+++ app/index.php
@@ -418,6 +418,16 @@
 
 
 				});
+				function normalize(val, min, max){
+				  if(min < 0){
+					max += 0 - min;
+					val += 0 - min;
+					min = 0;
+				  }
+				  val = val - min;
+				  max = max - min;
+				  return Math.max(0, Math.min(1, val / max));
+				}
 			</script>
 			<br/>
 			<h1>
