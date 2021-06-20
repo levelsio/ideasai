68ad8192ae5669daf9afdbf51f16f79c53f74112
levelsio <7150848+levelsio@users.noreply.github.com>
2021-06-20 16:19:33 +0700

    x

--- app/index.php
+++ app/index.php
@@ -867,7 +867,7 @@
 			<a name="readme"></a>
 
 			<p clas="text" style="text-align:left;line-height:1.8;">
-				The idea (lol) is to give you inspiration to make something cool, if you lack inspiration right now. Many ideas here might not be perfect but they might give you the spark to start thinking to get to a really good idea further on.
+				The idea (no pun intended) is to give you inspiration to make something cool, if you lack inspiration right now. Many ideas here might not be perfect but they might give you the spark to start thinking to get to a really good idea further on.
 			</p>
 			<p clas="text" style="text-align:left;line-height:1.8;">
 				Please ❤️ like ideas that you feel are promising and ❌ dislike ideas that are bad, too vague, too obvious, or already exist. Ideas and their rating are fed back into OpenAI, so you (and others) constantly train the model what good and bad ideas are which improves the next ideas it comes up with! At least, that's the idea (aye!).
