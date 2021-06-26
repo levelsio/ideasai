ceef7f6518d7a8a4c69a08b5087260f7309f4073
levelsio <7150848+levelsio@users.noreply.github.com>
2021-06-27 01:59:31 +0700

    x

--- app/index.php
+++ app/index.php
@@ -1004,8 +1004,6 @@
 				<br/>
 
 
-				<hr>
-
 				<style>
 					footer {
 						background: #fff;
@@ -1018,6 +1016,7 @@
 					.tag-container {
 						max-width: 1100px;
 	    				width: 100%;
+	    				margin: 0 auto;
 					}
 					.tag-link {
 					    text-align: left;
